f9de48fc3e40a0030b6668184b12eb1daddd46eb
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-04-19 19:27:20 -0300

    tasks manipulating arrays

---
+++ Guia-estelar-Javascript/manipulando-arrays/script-2.js
@@ -0,0 +1,11 @@
+/*
+    Para transformar uma string em um array, pode-se usar o objeto Array com o método from(), 
+    passando como argumento a string a ser transformada em array. Por exemplo console.log(Array.from("developer")
+*/
+
+let word = "manipulação"
+console.log(Array.from(word))
+
+
+
+
---
+++ Guia-estelar-Javascript/manipulando-arrays/script.js
@@ -0,0 +1,31 @@
+/*
+Para contar a quantidade de elementos em um array pode-se usar o método length.
+*/
+
+/*console.log([
+    "a",
+    {type: "array"},
+    function() {return "alo"},
+].length)*/
+
+/*console.log([
+    "a",
+    {type: "array"},
+    function() {return "alo"},
+][2])*/
+
+// console.log([
+//     "a",
+//     {type: "array"},
+//     function() {return "alo"},
+// ][2]())
+
+console.log([
+    "a",
+    {type: "array"},
+    function() {return "alo"},
+][1].type)
+
+
+
+
