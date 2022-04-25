191b6fa6f6d91e8a7b3d1f4601af692fdb9f645c
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-04-25 15:59:44 -0300

    adding changes and files of taks

--- Guia-estelar-Javascript/manipulando-arrays/script-3-exercitando.js
+++ Guia-estelar-Javascript/manipulando-arrays/script-3-exercitando.js
@@ -13,3 +13,20 @@
 */
 
 
+// MANIPULANDO ARRAYS
+
+let techs =  ["html", "css", "js"]
+// Adicionar um item no fim
+techs.push("nodejs")
+// adicionar no começo
+techs.unshift("sql")
+// remover do fim
+techs.pop()
+// remover do começo
+techs.shift()
+// pegar somente alguns elementos do array
+console.log(techs.slice(1, 3))
+// remover 1 ou mais intens em qualquer posição do array
+// encontrar a posição de um elemento
+
+console.log(techs)
