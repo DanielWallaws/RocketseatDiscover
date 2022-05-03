b0b5fd1cfa0413b11fb4f23058e7de85f53a7ba9
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-05-03 14:09:12 -0300

    updating data

---
+++ Guia-estelar-Javascript/praticar-para-aperfeicoar/sistema-nota.js
@@ -0,0 +1,13 @@
+/*
+    Crie um algoritmo que transforme
+    as notas do sistema numérico para 
+    sistema de notas em caracteres tipo A B C
+
+    * de 90 para cima - A
+    * entre 80 e 89 - B
+    * entre 70 e 79 - C
+    * entre 60 e 69 - D
+    * menor que 60 - F
+    * 
+*/
+
