57eba155b613a052c5eafc3e625a84368635d5a2
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-05-05 14:05:31 -0300

    task two complet

--- Guia-estelar-Javascript/praticar-para-aperfeicoar/fluxo-caixa.js
+++ Guia-estelar-Javascript/praticar-para-aperfeicoar/fluxo-caixa.js
@@ -27,41 +27,32 @@ function sum(array) {
     return total
 }
 
-function calculateBalance() {
-    const calculateRevenue = sum(cashFlow.revenue)
-    const calculateExpenditure =sum(cashFlow.expenditure)
-    let balancefinal = calculateRevenue-calculateExpenditure
-    
-    if(balancefinal >=0) {
-        console.log(`Seu saldo é positivo. Você possui R$ ${balancefinal.toFixed[2]}`)
-    }
-}
-
 function addRevenue(value) {
     cashFlow.revenue.push(value)
 }
 
-function removeRevenue(indice, theamount, item) {
-    cashFlow.revenue.splice(indice, theamount, item)
+function removeRevenue(index, quantity_items_remove) {
+    cashFlow.revenue.splice(index, quantity_items_remove)
 }
 
 function addExpenditure(value) {
     cashFlow.expenditure.push(value)
 }
 
-function removeExpenditure (indice, theamount, item) {
-    cashFlow.expenditure.slice(indice, theamount, item)
+function removeExpenditure (index, quantity_items_remove) {
+    cashFlow.expenditure.slice(index, quantity_items_remove)
 }
 
-
-
-
-// function removeRevenue () {
-//     cashFlow.revenue.splice()
-// }
-
-// function calculefamilyBalance() {
-    //}
-
-
-// console.log(calculefamilyBalance(50, 30));
+function calculateBalance() {
+    const calculateRevenue = sum(cashFlow.revenue)
+    const calculateExpenditure = sum(cashFlow.expenditure)
+    const balancefinal = calculateRevenue-calculateExpenditure
+    
+    if(balancefinal >0) {
+        console.log(`Seu saldo é positivo. Você possui R$ ${balancefinal.toFixed(2)}`)
+    } else if (balancefinal <0) {
+        console.log(`Seu saldo é negativo. Você possui R$ ${balancefinal.toFixed(2)}`)
+    } else{
+        console.log(`Seu saldo é de R$ ${balancefinal.toFixed(2)}`)
+    }
+}
