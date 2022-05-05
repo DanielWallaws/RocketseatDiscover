8574b47d7905b4ef13a069cd360a4fae7342c568
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-05-04 22:56:51 -0300

    uploading task two before tests

--- Guia-estelar-Javascript/praticar-para-aperfeicoar/fluxo-caixa.js
+++ Guia-estelar-Javascript/praticar-para-aperfeicoar/fluxo-caixa.js
@@ -17,21 +17,42 @@ let cashFlow = {
     "expenditure": []
 }
 
-function addRevenue (value) {
+function sum(array) {
+    let total =0;
+
+    for(let value of array) {
+        total += value
+    }
+
+    return total
+}
+
+function calculateBalance() {
+    const calculateRevenue = sum(cashFlow.revenue)
+    const calculateExpenditure =sum(cashFlow.expenditure)
+    let balancefinal = calculateRevenue-calculateExpenditure
+    
+    if(balancefinal >=0) {
+        console.log(`Seu saldo é positivo. Você possui R$ ${balancefinal.toFixed[2]}`)
+    }
+}
+
+function addRevenue(value) {
     cashFlow.revenue.push(value)
 }
 
-function removeRevenue (number, ) {
-    cashFlow.revenue.splice()
+function removeRevenue(indice, theamount, item) {
+    cashFlow.revenue.splice(indice, theamount, item)
 }
 
-function addExpenditure (value) {
+function addExpenditure(value) {
     cashFlow.expenditure.push(value)
 }
 
-function removeExpenditure (value) {
-    cashFlow.expenditure.slice(value)
+function removeExpenditure (indice, theamount, item) {
+    cashFlow.expenditure.slice(indice, theamount, item)
 }
+
 
 
 
