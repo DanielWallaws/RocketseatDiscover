d20c7be8cfc61c16f1d2967122b0fd91bf10f07a
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2022-11-17 18:44:34 -0300

    uploading data of taks object orientation

---
+++ Guia-estelar-Javascript/orientacao-objeto/estrutural-vs-poo.js
@@ -0,0 +1,20 @@
+//Orientação a Objeto
+class Poligono {
+    constructor(altura, largura) {
+        this.altura = altura
+        this.largura = largura
+    }
+
+    get area() {
+        return this.#calcularArea()
+    }
+
+    #calcularArea() {
+        return this.altura * this.largura
+    }
+}
+
+//Criar o objeto
+let poligono = new Poligono(50,60)
+
+console.log(poligono.area)
